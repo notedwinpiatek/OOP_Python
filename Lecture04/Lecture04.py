12e96871b71a339717dcf9517eac19687d3c0dfc
Edwin Piątek <92691520+notedwinpiatek@users.noreply.github.com>
2024-04-19 12:32:23 +0200

    unittest skip & fail

--- Lecture04/Lecture04.py
+++ Lecture04/Lecture04.py
@@ -1,13 +1,18 @@
 # test_port1.py
 
-import unittest
-from portfolio1 import Portfolio
+# import unittest
+# from portfolio1 import Portfolio
 
-class PortfolioTest(unittest.TestCase):
-    def test_buy_one_stock(self):
-        p = Portfolio()
-        p.buy("IBM", 100, 176.48)
-        assert p.cost() == 17648.0
+# class PortfolioTest(unittest.TestCase):
+#     def test_buy_one_stock(self):
+#         p = Portfolio()
+#         p.buy("IBM", 100, 176.48)
+#         assert p.cost() == 17648.0
         
         
 # $ python - m unittest test_port1
+# class Test():
+#     @unittest.skip("Not done yet")
+#         def test():
+#             pass
+    # @unittest.fail("Write this test!")

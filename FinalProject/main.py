d1ed97eb1798422739dc81175f1edb485d0550b7
Edwin Piątek <92691520+notedwinpiatek@users.noreply.github.com>
2024-05-07 19:40:00 +0200

    feature: skin change on jump

--- FinalProject/main.py
+++ FinalProject/main.py
@@ -10,6 +10,8 @@ score_font = pygame.font.Font('font/Warzone.ttf', 15)
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
+ground_rectangle = ground_surface.get_rect(topleft = (0,300))
+
 text_surface = text_font.render('BOunce King', False, 'Black' )
 text_rectangle = text_surface.get_rect(center = (400,50))
 
@@ -21,21 +23,23 @@ snail_rectangle = snail_surface.get_rect(midbottom = (850,300))
 
 player_surface = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
 player_rectangle = player_surface.get_rect(midbottom = (80,300))
+player_gravity = 0
  
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        # if event.type == pygame.MOUSEMOTION:
-        #     if player_rectangle.collidepoint(event.pos):
-        #         print("mouse on player")
-            
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player_surface = pygame.image.load('graphics/Player/jump.png').convert_alpha()
+                player_gravity = -20
+        # if event.type == pygame.KEYUP:
+        #     if event.key == pygame.K_SPACE:
+        #         player_surface = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+
     screen.blit(sky_surface, (0, 0))
-    screen.blit(ground_surface, (0, 300))
-    pygame.draw.rect(screen, "White", text_rectangle)
-    # pygame.draw.line(screen,"Red", (0,0), (800,400),10)
-    pygame.draw.ellipse(screen, '#ffdd00', pygame.Rect(50, 200, 100, 100))
+    screen.blit(ground_surface, ground_rectangle)
     screen.blit(text_surface, (text_rectangle))
     screen.blit(score_surface, score_rectangle)
     snail_rectangle.x -= 3
@@ -43,13 +47,14 @@ while True:
         snail_rectangle.left = 800
     screen.blit(snail_surface, snail_rectangle)
     screen.blit(player_surface, player_rectangle)
-            
-    # if player_rectangle.colliderect(snail_rectangle):
-    #     print("Collision")
 
-    # mouse_position = pygame.mouse.get_pos()
-    # if player_rectangle.collidepoint(mouse_position):
-    #     print("mouse on player")
+
+    player_gravity +=1
+    player_rectangle.y += player_gravity
+    if player_rectangle.y >= 220:
+        player_rectangle.y = 220
+        player_gravity = 0
+        player_surface = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
 
     pygame.display.update() 
     clock.tick(60)

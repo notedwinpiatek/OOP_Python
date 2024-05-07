f49dd1858dcfe294283e931d5dc83f67576f56ea
Edwin Piątek <92691520+notedwinpiatek@users.noreply.github.com>
2024-05-07 12:23:25 +0200

    font folder

--- FinalProject/main.py
+++ FinalProject/main.py
@@ -2,13 +2,14 @@ import pygame
 from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Bounce King')
 clock = pygame.time.Clock()
-# test_font = pygame.font.Font()
+test_font = pygame.font.Font(None, 50)
 
 sky_surface = pygame.image.load('graphics/Sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
+text_surface = test_font.render('My game', False, 'Green' )
 # # test_surface = pygame.Surface((100, 200))
 # # test_surface.fill('Red')
  
@@ -19,7 +20,8 @@ while True:
             exit()
             
     screen.blit(sky_surface, (0, 0))
-    screen.blit(ground_surface, (0, 400))
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (300,50))
             
     pygame.display.update()
     clock.tick(60)

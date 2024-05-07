3da21d8fae7f505e54b1e18e0abe06e1882f47fa
Edwin Piątek <92691520+notedwinpiatek@users.noreply.github.com>
2024-05-07 10:23:56 +0200

    pygame

--- FinalProject/main.py
+++ FinalProject/main.py
@@ -1,2 +1,21 @@
-import pygame.examples.aliens as aliens
-aliens.main()
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Bounce King')
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((100, 200))
+test_surface.fill('Red')
+ 
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+            
+    screen.blit(test_surface, (200, 100))
+            
+    pygame.display.update()
+    clock.tick(60)

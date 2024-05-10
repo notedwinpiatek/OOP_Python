65062953a54d1e997d82c7c91bfa5ce7d26fc121
Edwin Piątek <92691520+notedwinpiatek@users.noreply.github.com>
2024-05-10 11:33:25 +0200

    fix: emptying obstacle group

--- FinalProject/main.py
+++ FinalProject/main.py
@@ -48,7 +48,7 @@ class Obsticle(pygame.sprite.Sprite):
         if type == 'fly':
             fly_1 = pygame.image.load('graphics/Fly/Fly1.png').convert_alpha()
             fly_2 = pygame.image.load('graphics/Fly/Fly2.png').convert_alpha()
-            self.frames = [fly_1,fly_2]
+            self.frames = [fly_1, fly_2]
             y_pos = 210
         else:
             snail_1 = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
@@ -58,11 +58,11 @@ class Obsticle(pygame.sprite.Sprite):
 
 
         self.animation_index = 0
-        self.image = self.frames
+        self.image = self.frames[self.animation_index]
         self.rect = self.image.get_rect(midbottom = (randint(900,1100),y_pos))
 
     def animation_state(self):
-        self.animation_index += 0,1
+        self.animation_index += 0.1
         if self.animation_index >= len(self.frames): self.animation_index = 0
         self.image = self.frames[int(self.animation_index)]
     
@@ -95,6 +95,7 @@ def collisions(player,obstacles):
     if obstacles:
         for obstacle_rect in obstacles:
             if player.colliderect(obstacle_rect):
+                obsticle_group.empty()
                 return False
     return True
 
